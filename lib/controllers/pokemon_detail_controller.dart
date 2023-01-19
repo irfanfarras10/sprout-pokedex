3cc9e8c4211902a2c94ee71268a96c8b5efc85e8
Irfan Farras <irfanfarras10@gmail.com>
2023-01-19 15:14:32 +0700

    feat(detail): implement detail data with image

--- lib/controllers/pokemon_detail_controller.dart
+++ lib/controllers/pokemon_detail_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:sprout_pokedex/models/pokemon_detail_api_response_model.dart';
 import 'package:sprout_pokedex/services/pokemon_api_service.dart';
 
 enum ViewState {
@@ -11,6 +12,7 @@ class PokemonDetailController extends GetxController {
   final PokemonApiService apiService = PokemonApiService();
   Rx<ViewState> viewState = ViewState.loading.obs;
   int id = int.parse(Get.parameters['id']!);
+  PokemonDetailApiResponseModel data = PokemonDetailApiResponseModel();
 
   @override
   void onInit() async {
@@ -21,6 +23,7 @@ class PokemonDetailController extends GetxController {
   Future<void> loadData() async {
     await apiService.getPokemonDetailData(id).then((apiResponse) {
       print(apiResponse);
+      data = PokemonDetailApiResponseModel.fromJson(apiResponse);
       viewState.value = ViewState.finish;
     });
   }
--- lib/screens/pokemon_detail_screen.dart
+++ lib/screens/pokemon_detail_screen.dart
@@ -22,10 +22,149 @@ class PokemonDetailScreen extends StatelessWidget {
             body = Column(
               children: [
                 Expanded(
-                  child: Center(child: Text('Atas')),
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/backgrounds/pokemon_list_background.png',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent.withOpacity(0.3),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                        25,
+                                        60,
+                                        25,
+                                        0,
+                                      ),
+                                      child: Text(
+                                        '${controller.data.name!}',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                        25,
+                                        10,
+                                        25,
+                                        0,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: List.generate(
+                                          controller.data.types!.length,
+                                          (index) {
+                                            return Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                0,
+                                                0,
+                                                10,
+                                                0,
+                                              ),
+                                              child: Chip(
+                                                label: Text(
+                                                  '${controller.data.types![index].type!.name}',
+                                                  style: TextStyle(
+                                                    fontSize: 16.0,
+                                                    fontWeight: FontWeight.w600,
+                                                  ),
+                                                ),
+                                                backgroundColor:
+                                                    Colors.blueAccent.shade100,
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                    25,
+                                    90,
+                                    25,
+                                    0,
+                                  ),
+                                  child: Text(
+                                    '#${controller.id.toString().padLeft(3, '0')}',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Expanded(
+                              child: Center(
+                                child: SizedBox(
+                                  child: Image.network(
+                                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${controller.id}.png',
+                                    fit: BoxFit.fitHeight,
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      } else {
+                                        return Center(
+                                          child: CircularProgressIndicator(
+                                            color: Colors.red,
+                                          ),
+                                        );
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 Expanded(
-                  child: Center(child: Text('Bawah')),
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent.withOpacity(0.3),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50.0),
+                            topRight: Radius.circular(50.0),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ],
             );
